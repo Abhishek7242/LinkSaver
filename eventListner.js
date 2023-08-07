fae43cbdaa340a3ef5816a0bd7d08fdf4d7ff42b
Abhishek Verma <131652685+Abhishek7242@users.noreply.github.com>
2023-08-07 09:39:55 +0530

    updated
    added night mode

--- eventListner.js
+++ eventListner.js
@@ -42,22 +42,116 @@ addNewBox.addEventListener('click', () => {
         inputBox.style.opacity = 0
 
     })
-let nightMode=document.getElementById('nightMode')
+let moonStarFill = document.querySelector('.bi-brightness-high-fill')
+let moonStar = document.querySelector('.bi-moon-stars')
+let box = document.querySelector('.box')
+
+let nightMode = document.getElementById('nightMode')
+let nightModeText = document.getElementById('nightModeText')
+let mode = sessionStorage.getItem('mode')
+console.log(mode)
+// if (mode === 'nightMode') {
+//     moonStar.style.display = 'inline'
+//     moonStarFill.style.display = 'none'
+//     mainContainer.style.background = 'white'
+//     markedContainer.style.background = 'white'
+//     section1.style.background = 'white'
+//     nightMode.style.color = 'black'
+moonStarFill.style.display = 'none'
+// }
+if (mode === 'lightmode') {
+    moonStar.style.display = 'inline'
+    moonStarFill.style.display = 'none'
+    mainContainer.style.background = 'white'
+    markedContainer.style.background = 'white'
+    section1.style.background = 'white'
+    nightMode.style.color = 'black'
+    nightModeText.innerText = 'Night mode'
+    box.style.background = 'none'
+
+    let container = document.getElementById('container');
+    let addBtns = container.querySelectorAll('.addBtn');
+    let markcontainer = document.getElementById('markcontainer');
+    let addBtns2 = markcontainer.querySelectorAll('.addBtn');
+
+    // Convert NodeList to an array and then use forEach
+    Array.from(addBtns).forEach(function (addBtn) {
+        addBtn.classList.add('bS')
+        addBtn.classList.remove('bSl')
+    });
+    
+    Array.from(addBtns2).forEach(function (addBtn) {
+        addBtn.classList.add('bS')
+        addBtn.classList.remove('bSl')
+    });
+    
+    
+} else if(mode==='nightmode') {
+    moonStar.style.display = 'none'
+    moonStarFill.style.display = 'inline'
+    mainContainer.style.background = 'black'
+    markedContainer.style.background = 'black'
+    section1.style.background = 'black'
+    nightMode.style.color = 'white'
+    box.style.background = 'white'
+    nightModeText.innerText = 'Light mode'
+
+    let container = document.getElementById('container');
+    let addBtns = container.querySelectorAll('.addBtn');
+    let markcontainer = document.getElementById('markcontainer');
+    let addBtns2 = markcontainer.querySelectorAll('.addBtn');
+
+    // Convert NodeList to an array and then use forEach
+    Array.from(addBtns).forEach(function (addBtn) {
+        addBtn.classList.add('bSl')
+        addBtn.classList.remove('bS')
+    });
+    
+    Array.from(addBtns2).forEach(function (addBtn) {
+        addBtn.classList.add('bSl')
+        addBtn.classList.remove('bS')
+    });
+
+
+    
+    
+}
+
+
 nightMode.addEventListener('click', () => {
-
-
+    let mainContainer=document.getElementById('mainContainer')
+    
     let tag = document.querySelector('#nightMode svg')
     console.log(tag)
-    let moonStarFill = document.querySelector('.bi-moon-stars-fill')
+    let moonStarFill = document.querySelector('.bi-brightness-high-fill')
     let moonStar = document.querySelector('.bi-moon-stars')
-    if (window.getComputedStyle(moonStar).display !== 'none') {
+    if (window.getComputedStyle(moonStar).display !== 'none'||mode==='lightmode') {
         moonStar.style.display ='none'
         moonStarFill.style.display = 'inline'
         mainContainer.style.background = 'black'
         markedContainer.style.background = 'black'
         section1.style.background = 'black'
         nightMode.style.color = 'white'
-        
+        nightModeText.innerText='Light mode'
+        box.style.background = 'white'
+
+        let container = document.getElementById('container');
+        let addBtns = container.querySelectorAll('.addBtn');
+        let markcontainer = document.getElementById('markcontainer');
+        let addBtns2 = markcontainer.querySelectorAll('.addBtn');
+
+        // Convert NodeList to an array and then use forEach
+        Array.from(addBtns).forEach(function (addBtn) {
+            addBtn.classList.add('bSl')
+            addBtn.classList.remove('bS')
+        });
+
+        Array.from(addBtns2).forEach(function (addBtn) {
+            addBtn.classList.add('bSl')
+            addBtn.classList.remove('bS')
+        });
+
+        sessionStorage.setItem('mode','nightmode')
     } else {
         
         moonStar.style.display ='inline'
@@ -66,6 +160,26 @@ nightMode.addEventListener('click', () => {
         markedContainer.style.background = 'white'
         section1.style.background = 'white'
         nightMode.style.color = 'black'
+        nightModeText.innerText = 'Night mode'
+        box.style.background = 'none'
+
+        let container = document.getElementById('container');
+        let addBtns = container.querySelectorAll('.addBtn');
+        let markcontainer = document.getElementById('markcontainer');
+        let addBtns2 = markcontainer.querySelectorAll('.addBtn');
+
+        // Convert NodeList to an array and then use forEach
+        Array.from(addBtns).forEach(function (addBtn) {
+            addBtn.classList.add('bS')
+            addBtn.classList.remove('bSl')
+        });
+
+        Array.from(addBtns2).forEach(function (addBtn) {
+            addBtn.classList.add('bS')
+            addBtn.classList.remove('bSl')
+        });
+        sessionStorage.setItem('mode', 'lightmode')
+
     }
     
 
